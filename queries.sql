70836aacdbf32f7de764ba235c974cf2b4edec4c
Bryan Bilek <bbilek35@gmail.com>
2020-04-13 10:34:50 -0500

    added a stretch query

--- queries.sql
+++ queries.sql
@@ -27,3 +27,5 @@ update Customers set PostalCode='11122' where ContactName='Bilbo Baggins'
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
+
+select SupplierName from Suppliers where length(SupplierName) > 20;
