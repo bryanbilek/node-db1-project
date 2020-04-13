d69b5c59d5644601907ffe57caec24687fb6f820
bryanbilek <bbilek35@gmail.com>
2020-04-13 00:19:05 -0500

    added CRUD for accounts endpoint

--- api/server.js
+++ api/server.js
@@ -4,6 +4,10 @@ const db = require("../data/dbConfig.js");
 const accountsRouter = require("../accounts/accountsRouter");
 
 server.use(express.json());
-server.use('/api/accounts', accountsRouter);
+server.use("/api/accounts", accountsRouter);
+
+server.get("/", (req, res) => {
+    res.send("<h2>SQL & Knex!</h2>");
+})
 
 module.exports = server;
