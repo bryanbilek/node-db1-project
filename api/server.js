6675b958a0a359ac8dd916fa6c84540eb6ab131c
bryanbilek <bbilek35@gmail.com>
2020-04-12 22:41:36 -0500

    wrote sql queries, added accountsrouter (to server, too)

--- api/server.js
+++ api/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-
-const db = require("../data/dbConfig.js");
-
 const server = express();
+const db = require("../data/dbConfig.js");
+const accountsRouter = require("../accounts/accountsRouter");
 
 server.use(express.json());
+server.use('/api/accounts', accountsRouter);
 
 module.exports = server;
